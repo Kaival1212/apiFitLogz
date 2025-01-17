b992cb2c7b2e4b5fc76746fc3c344a93eb448db8
Kaival <87368466+Kaival1212@users.noreply.github.com>
2025-01-17 17:33:13 +0000

    added openfood facts api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
+use GuzzleHttp\Client;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -78,4 +80,58 @@ Route::post("/register", function (Request $request) {
 Route::middleware(['auth:sanctum'])->post('/logout', function (Request $request) {
     $request->user()->tokens()->delete();
     return response()->json(['message' => 'Logged out']);
+});
+
+Route::get('/openfoodfacts/product/{id}', function (String $id) {
+
+    info("Fetching product with id: $id");
+
+    $client = new Client();
+    $url = "https://api.openfoodfacts.org/api/v3/product/$id.json";
+
+    $response = $client->get($url);
+    $jsondata = $response->getBody()->getContents();
+    $data = json_decode($jsondata,true);
+    $product = $data["product"];
+
+
+    info($product);
+
+
+
+    return response()->json([
+        'id' => $product["_id"],
+        'name' => $product["product_name"],
+        'name_en' => $product["product_name_en"],
+        'quantity' => $product["product_quantity"],
+        'quantity_unit' => $product["product_quantity_unit"],
+        'image_front_small_url' => $product["image_front_small_url"],
+
+        'carbohydrates' => $product["nutriments"]["carbohydrates"] ?? null,
+        'carbohydrates_100g' => $product["nutriments"]["carbohydrates_100g"] ?? null,
+        'carbohydrates_serving' => $product["nutriments"]["carbohydrates_serving"] ?? null,
+        'carbohydrates_unit' => $product["nutriments"]["carbohydrates_unit"] ?? null,
+
+        'energy' => $product["nutriments"]["energy"] ?? null,
+        'energy-kcal' => $product["nutriments"]["energy-kcal"] ?? null,
+        'energy-kcal_100g' => $product["nutriments"]["energy-kcal_100g"] ?? null,
+        'energy-kcal_serving' => $product["nutriments"]["energy-kcal_serving"] ?? null,
+        'energy-kcal_unit' => $product["nutriments"]["energy-kcal_unit"] ?? null,
+
+        'fat' => $product["nutriments"]["fat"] ?? null,
+        'fat_100g' => $product["nutriments"]["fat_100g"] ?? null,
+        'fat_serving' => $product["nutriments"]["fat_serving"] ?? null,
+        'fat_unit' => $product["nutriments"]["fat_unit"] ?? null,
+
+        'proteins' => $product["nutriments"]["proteins"] ?? null,
+        'proteins_100g' => $product["nutriments"]["proteins_100g"] ?? null,
+        'proteins_serving' => $product["nutriments"]["proteins_serving"] ?? null,
+        'proteins_unit' => $product["nutriments"]["proteins_unit"] ?? null,
+
+        'sugar' => $product["nutriments"]["sugars"] ?? null,
+        'sugar_100g' => $product["nutriments"]["sugars_100g"] ?? null,
+        'sugar_serving' => $product["nutriments"]["sugars_serving"] ?? null,
+        'sugar_unit' => $product["nutriments"]["sugars_unit"] ?? null,
+    ]);
+
 });
